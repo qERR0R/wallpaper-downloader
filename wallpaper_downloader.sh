267768d3ee2f5ab00c7a8db22ba30123efb4bd9c
qERR0R <167422755+qERR0R@users.noreply.github.com>
2024-04-24 16:36:52 +0000

    Update wallpaper_downloader.sh

--- wallpaper_downloader.sh
+++ wallpaper_downloader.sh
@@ -1,50 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
+# Directory to store downloaded wallpapers
 walldir="${HOME}/Wallpapers/"
 mkdir -p "$walldir"
 count=0
 
+# Function to clean up and exit script
 cleanup() {
     if [ -n "$filename" ]; then
         rm -f "$walldir/$filename"
         printf "\nPartially downloaded image deleted: %s\n" "$filename"
-		printf "Download Complete!\n$count wallpapers downloaded!\n"
+        printf "Downloading Canceled!\n$count wallpapers downloaded!\n"
     fi
     exit 1
 }
 
+# Trap interrupt signal to call cleanup function
 trap cleanup INT
 
-get_results () {
+# Function to fetch search results from Wallhaven API
+get_results() {
     curl -s -G "https://wallhaven.cc/api/v1/search" \
         -d "q=$1" \
         -d "sorting=relevance" \
         -d "atleast=1920x1080" \
         -d "page=$2" |
-    jq -r '.data[]?|.path'
+    jq -r '.data[]? | .path'
 }
 
+# Check if query is provided as argument
 query="$1"
 [ -z "$query" ] && {
     printf "Query Not Specified\n" >&2
     exit 1
 }
 
-pages="$2"
-[ -z "$pages" ] && pages=1
+# Set default value for pages if not provided
+pages="${2:-1}"
 
-for ((page=1; page<=$pages; page++)); do
+# Loop through pages to download wallpapers
+for ((page = 1; page <= pages; page++)); do
     printf "Page Load %d...\n" "$page"
-    get_results "$query" "$page" | while read -r url; do
-		trap cleanup INT
+    # Read results from get_results function and process each URL
+    while read -r url; do
         filename=$(basename "$url")
-		if [ -e "$walldir/$filename" ]; then
-			printf "Skip: %s (Already downloaded)\n" "$filename"
-		else
-        	cd "$walldir" && wget -q --show-progress -O "$filename" "$url"
-			((count++))
-		fi
-    done
+        if [ -e "$walldir/$filename" ]; then
+            printf "Skip: %s (Already downloaded)\n" "$filename"
+        else
+            cd "$walldir" && wget -q --show-progress -O "$filename" "$url"
+            ((count++))
+        fi
+    done < <(get_results "$query" "$page")
 done
 
 printf "Download Complete!\n$count wallpapers downloaded!\n"
